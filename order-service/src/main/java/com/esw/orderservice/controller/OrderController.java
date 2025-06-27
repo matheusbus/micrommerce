2f7b8d14fae7025a5632774f66b96da413f9f27e
Matheus Buschermoehle <73143728+matheusbus@users.noreply.github.com>
2025-06-27 03:15:30 -0300

    fix(kafka): adjustment on docker network to kafka works

--- order-service/src/main/java/com/esw/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/esw/orderservice/controller/OrderController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/order")
+@RequestMapping("api/orders")
 public class OrderController {
 
     private final CreateOrderUseCase createOrderUseCase;
